cf5de075d23b52f9b9bf39ae303e2e51ee903033
coutinhogabriel <gabriel.coutin@hotmail.com>
2024-10-03 21:55:21 -0300

    03/10/2024v1

--- gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
+++ gui_exercicio1_calculadora_abas/src/main/java/com/example/CalculadoraSimples.java
@@ -3,17 +3,21 @@ package com.example;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculadoraSimples extends JPanel {
-    // atributos
+    // ATRIBUTOS
     JTextField displaySimples;
     String[] botoes = { "7", "8", "9", "/",
             "4", "5", "6", "*",
             "1", "2", "3", "-",
-            "0", "=", "+", "c" };
+            "0", "=", "+", "C" };
 
-    // construtor
+    // CONSTRUTOR
     public CalculadoraSimples() {
         super(new BorderLayout());
         displaySimples = new JTextField();
@@ -21,27 +25,66 @@ public class CalculadoraSimples extends JPanel {
         displaySimples.setEditable(false);
         this.add(displaySimples, BorderLayout.NORTH);
 
-        // Criar um pianel para os botões
+        // Criar um Painel para os botões
         JPanel painelBotoes = new JPanel(new GridLayout(4, 4, 3, 3));
         for (String textoBotoes : botoes) {
             JButton botao = new JButton(textoBotoes);
-            // botao.addActionListener(null);
+            botao.addActionListener(new Handler1());
             painelBotoes.add(botao);
         }
         this.add(painelBotoes, BorderLayout.CENTER);
     }
 
-    public void setDisplaySimples(String texto) {
+    // altera valor display
+    public void setDisplay(String texto) {
         this.displaySimples.setText(texto);
     }
 
-    public String getDisplaySimples() {
+    // pega valor display
+    public String getDisplay() {
         return displaySimples.getText();
     }
 
-    public class Handlerl implements ActionListener{
- 
-     }
+    public class Handler1 implements ActionListener {
+        double valorAtual;
+        String operador;
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String comando = e.getActionCommand();
+
+            
+            if (comando.matches("\\d")) {
+                displaySimples.setText(displaySimples.getText() + comando);
+            } else if (comando.matches("[+\\-*/]")) {
+                valorAtual = Double.parseDouble(displaySimples.getText());
+                operador = comando;
+                displaySimples.setText("");
+            } else if (comando.equals("=")) {
+                double segundoValor = Double.parseDouble(displaySimples.getText());
+                double resultado = 0;
+
+                switch (operador) {
+                    case "+":
+                        resultado = valorAtual + segundoValor;
+                        break;
+                    case "-":
+                        resultado = valorAtual - segundoValor;
+                        break;
+                    case "*":
+                        resultado = valorAtual * segundoValor;
+                        break;
+                    case "/":
+                        resultado = valorAtual / segundoValor;
+                        break;
+                }
+                displaySimples.setText(String.valueOf(resultado));
+            } else if (comando.equals("C")) {
+                displaySimples.setText("");
+                valorAtual = 0;
+            }
+
+        }
+    }
 }
- 
